5b43cf4f0a813d594dae63bcfb224901ef9df581
Peterabk <peter.abk@outlook.com>
2024-11-13 18:40:24 -0500

    making nav2 working without map and on gps

--- bumperbot_ws/src/bumperbot_controller/launch/controller.launch.py
+++ bumperbot_ws/src/bumperbot_controller/launch/controller.launch.py
@@ -1,8 +1,11 @@
+import os
+from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction, OpaqueFunction
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import UnlessCondition, IfCondition
+from launch.actions import IncludeLaunchDescription
 
 
 def noisy_controller(context, *args, **kwargs):
@@ -119,6 +122,21 @@ def generate_launch_description():
         ]
     )
 
+    bumperbot_controller_pkg = get_package_share_directory("bumperbot_controller")
+    twist_mux_launch = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("twist_mux"),
+            "launch",
+            "twist_mux_launch.py"
+        ),
+        launch_arguments={
+            "cmd_vel_out": "bumperbot_controller/cmd_vel_unstamped",
+            "config_topics": os.path.join(bumperbot_controller_pkg, "config", "twist_mux_topics.yaml"),
+            "config_locks": os.path.join(bumperbot_controller_pkg, "config", "twist_mux_locks.yaml"),
+            "config_joy": os.path.join(bumperbot_controller_pkg, "config", "twist_mux_joy.yaml"),
+        }.items()
+    )
+
     noisy_controller_launch = OpaqueFunction(function=noisy_controller)
 
     return LaunchDescription(
@@ -132,6 +150,7 @@ def generate_launch_description():
             joint_state_broadcaster_spawner,
             wheel_controller_spawner,
             simple_controller,
+            twist_mux_launch,
             noisy_controller_launch,
         ]
     )
